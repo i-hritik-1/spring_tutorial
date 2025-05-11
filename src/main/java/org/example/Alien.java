be462d3957214b170cba530cabefa69d5a71d8f0
Hritik Raushan <official.ihritik@gmail.com>
2025-05-11 22:33:57 +0530

    Constructor Injection

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -4,14 +4,26 @@ import org.springframework.stereotype.Component;
 
 //@Component
 public class Alien {
+    private  int age;
+    private Laptop lap;
 
     public Alien()
     {
-        System.out.println("Object Created.");
+        System.out.println("Alien object created.");
     }
+
+    public Alien(int age, Laptop lap)
+    {
+        System.out.println("Constructor Called");
+        this.age = age;
+        this.lap = lap;
+    }
+
+
 
     public void code(String name)
     {
-        System.out.println("Alien "  + name + " is coding right now..... ");
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+        lap.compile();
     }
 }
--- src/main/java/org/example/Laptop.java
+++ src/main/java/org/example/Laptop.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Component;
 public class Laptop {
 
     public Laptop() {
+
         System.out.println("Laptop object created");
     }
 
     public void compile(){
+
         System.out.println("Compiling laptop for Alien");
     }
 }
