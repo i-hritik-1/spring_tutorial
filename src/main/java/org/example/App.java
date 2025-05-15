7d95eb815c76c0ef3cfa64df37fa54e92942acde
Hritik Raushan <official.ihritik@gmail.com>
2025-05-16 05:16:19 +0530

    java based config bean annotaion and configuration annotation

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,17 +1,31 @@
 package org.example;
 
+import org.example.config.AppConfig;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 
 public class App 
 {
     public static void main( String[] args )
     {
+        // Java Based Configuration
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Desktop desk = context.getBean(Desktop.class);
+        desk.compile();
+
+    }
+}
+
+
+/*
+XML based configuration
+    import org.springframework.context.ApplicationContext;
+    import org.springframework.context.support.ClassPathXmlApplicationContext;
 
         ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
         Alien obj = (Alien) context.getBean("alien");
         obj.code("Hritik");
 
-    }
-}
+ */
--- src/main/java/org/example/Desktop.java
+++ src/main/java/org/example/Desktop.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Desktop implements Computer{
 
+    public Desktop()
+    {
+        System.out.println("Desktop Object Created....");
+    }
+
     @Override
     public void compile()
     {
